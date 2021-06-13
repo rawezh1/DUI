4d4149f1a688a2081c3142bb3bd2302d00c9f465
Rawezh <rawezhmarif@gmail.com>
2021-06-13 12:07:35 +0300

    initial setup

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("It's alive!");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9EVUkvLi9zcmMvaW5kZXguanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJJdCdzIGFsaXZlIVwiKTtcbiJdLCJzb3VyY2VSb290IjoiIn0=
